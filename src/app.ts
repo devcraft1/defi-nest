2f555833d406482ff69cd38010652f0ef620c98a
Dennis Ukonu <ukonudennis64@gmail.com>
2022-12-23 07:08:39 +0100

    --readMe

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+
+// interface obj {
+//     user : string,
+//     name : string
+// }
+
+// function x <obj>(s:obj):obj{
+//  return s
+// }
+
+// let xx = x({user:"ee",name: "dd"})
+
+// console.log(xx)
+
+
+function id<T>(val:T):T{
+return
+}
